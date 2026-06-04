2ce8099a580a23fb6f062b370b223d7474a8c55d
Ashish Jha <ashishxyzjha@gmail.com>
2026-06-04 18:57:06 +0530

    Homepage Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Eventcard from "@/components/Eventcard";
+import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
+
 const Page = () => {
   return (
     <section>
@@ -7,6 +11,20 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in one place
       </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <Eventcard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
